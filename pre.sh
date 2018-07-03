d54441d98ba9543ba2dac838754f5eca4f830575
goodje <email@example.com>
2018-07-03 18:47:20 +0800

    support for permission sync

--- pre.sh
+++ pre.sh
@@ -7,6 +7,22 @@ SWORD_CUSTOM_PATH=${SWORD_PATH}/custom
 # export $SWORD_PATH
 # export $SWORD_CUSTOM_PATH
 
+# ## todo check distribution version
+case `uname -s` in
+    Darwin*)
+        os=mac
+        ;;
+    Linux*)
+        os=linux
+        ;;
+    # CYGWIN*, MINGW*, MSYS*, ..
+    *)
+        echo "doesn't support your operating system."
+        exit -1
+esac
+
+export os
+
 # -- function
 source_include () {
     [[ -f $1 ]] && source $1
